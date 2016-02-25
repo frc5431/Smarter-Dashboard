93275e6201bf19f57028e2fbc7597160504fc0c4
David Smerkous <smerkousdavid@gmail.com>
2016-02-25 17:18:55 -0600

    created window

--- Smarter-Dashboard/src/org/usfirst/frc/team5431/RobotDashboard.java
+++ Smarter-Dashboard/src/org/usfirst/frc/team5431/RobotDashboard.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team5431;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -13,6 +16,18 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 import org.usfirst.frc.team5431.components.IntakeCameraViewer;
 import org.usfirst.frc.team5431.components.RobotDisplay;
+
+import com.jumbo.components.JumboColor;
+import com.jumbo.components.entities.graphics.JumboImage;
+import com.jumbo.components.entities.graphics.JumboText;
+import com.jumbo.components.entities.graphics.JumboTextBox;
+import com.jumbo.rendering.Jumbo;
+import com.jumbo.rendering.JumboLaunchConfig;
+import com.jumbo.rendering.JumboScene;
+import com.jumbo.rendering.JumboTexture;
+import com.jumbo.tools.JumboSettings;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class RobotDashboard {
 	public static boolean recievedMessage = false;
@@ -29,79 +44,85 @@ public class RobotDashboard {
 		// shooting.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// shooting.setLayout(null);
 		// shooting.setVisible(true);
-
-		final JFrame frame = new JFrame("Team 5431 - Smarter Dashboard");
-		frame.setSize(2160, 784);
-		frame.setIconImage(SmarterDashboard.getImage("res" + File.separator + "logo.png"));
-		frame.setResizable(false);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(null);
-		frame.setVisible(true);
-
-		final JLabel connection = new JLabel("Starting up...", SwingConstants.CENTER);
-		connection.setBackground(Color.YELLOW);
-		connection.setBounds(750, 650, 1410, 50);
-		connection.setOpaque(true);
-		frame.add(connection);
-
-		final JLabel error = new JLabel("Connecting network tables...", SwingConstants.CENTER);
-		error.setBackground(Color.YELLOW);
-		error.setBounds(750, 700, 1410, 50);
-		error.setOpaque(true);
-		frame.add(error);
-
-		NetworkTable.setClientMode();
-		NetworkTable.setIPAddress("roborio-5431-frc.local");
-		SmarterDashboard.table = NetworkTable.getTable("5431");
-
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-				| UnsupportedLookAndFeelException e) {
-			e.printStackTrace();
-		}
-
-		// connection thread, updates once per second
-		exe.execute(new Thread() {
-			@Override
-			public void run() {
-				long lastTime = System.nanoTime();
-				double ns = 1000000000 / SmarterDashboard.CONNECTION_TPS;
-				// checks immediately for connection
-				double delta = 1;
-				while (true) {
-					long now = System.nanoTime();
-					delta += (now - lastTime) / ns;
-					lastTime = now;
-					if (delta >= 1) {
-						action();
-						delta--;
-					}
-				}
-			}
-
-			private void action() {
-				if (!SmarterDashboard.getConnectionStatus()) {
-					connection.setText("NO CONNECTION");
-					connection.setBackground(Color.RED);
-				} else {
-					connection.setText("Connected to Robot");
-					connection.setBackground(Color.GREEN);
-				}
-				error.setText(SmarterDashboard.table.getString("ERROR", "No error"));
-				SmarterDashboard.updateConnectionStatus(false);
-			}
+		
+		JumboSettings.logerrors=false;
+		Jumbo.setMainaction(()->{
+			final JumboScene scene = new JumboScene();
+			
+			final Rectangle connectionbounds = new Rectangle(750,50,1410,50);
+			final JumboImage connection = new JumboImage(new JumboTexture(JumboColor.YELLOW), connectionbounds);
+			scene.addEntity(connection);
+			final JumboTextBox connectiontext = new JumboTextBox(connectionbounds, new JumboText("Starting up..."));
+			scene.addEntity(connectiontext);
+			
+			Jumbo.setScene(scene);
 		});
+		Jumbo.start(new JumboLaunchConfig("Test", new Dimension(2160,784),new BufferedImage[]{SmarterDashboard.getImage("res" + File.separator + "logo.png")},"res"+File.separator+"liavishis"));
 
-		// new AxisCameraViewer(turret,exe);
-		// new LEDShower(shooting, exe);
-		// new MotorSettingser(settings,exe);
-		new RobotDisplay(frame, exe); // exe.execute(()->{
-		new IntakeCameraViewer(frame, exe);
-		// new AxisCameraViewer(turret,exe);
-		// });
-
-		// exe.execute(new USBCameraViewer(turret));
+//		final JLabel connection = new JLabel("Starting up...", SwingConstants.CENTER);
+//		connection.setBackground(Color.YELLOW);
+//		connection.setBounds(750, 650, 1410, 50);
+//		connection.setOpaque(true);
+//		frame.add(connection);
+//
+//		final JLabel error = new JLabel("Connecting network tables...", SwingConstants.CENTER);
+//		error.setBackground(Color.YELLOW);
+//		error.setBounds(750, 700, 1410, 50);
+//		error.setOpaque(true);
+//		frame.add(error);
+//
+//		NetworkTable.setClientMode();
+//		NetworkTable.setIPAddress("roborio-5431-frc.local");
+//		SmarterDashboard.table = NetworkTable.getTable("5431");
+//
+//		try {
+//			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+//				| UnsupportedLookAndFeelException e) {
+//			e.printStackTrace();
+//		}
+//
+//		// connection thread, updates once per second
+//		exe.execute(new Thread() {
+//			@Override
+//			public void run() {
+//				long lastTime = System.nanoTime();
+//				double ns = 1000000000 / SmarterDashboard.CONNECTION_TPS;
+//				// checks immediately for connection
+//				double delta = 1;
+//				while (true) {
+//					long now = System.nanoTime();
+//					delta += (now - lastTime) / ns;
+//					lastTime = now;
+//					if (delta >= 1) {
+//						action();
+//						delta--;
+//					}
+//				}
+//			}
+//
+//			private void action() {
+//				if (!SmarterDashboard.getConnectionStatus()) {
+//					connection.setText("NO CONNECTION");
+//					connection.setBackground(Color.RED);
+//				} else {
+//					connection.setText("Connected to Robot");
+//					connection.setBackground(Color.GREEN);
+//				}
+//				error.setText(SmarterDashboard.table.getString("ERROR", "No error"));
+//				SmarterDashboard.updateConnectionStatus(false);
+//			}
+//		});
+//
+//		// new AxisCameraViewer(turret,exe);
+//		// new LEDShower(shooting, exe);
+//		// new MotorSettingser(settings,exe);
+//		new RobotDisplay(frame, exe); // exe.execute(()->{
+//		new IntakeCameraViewer(frame, exe);
+//		// new AxisCameraViewer(turret,exe);
+//		// });
+//
+//		// exe.execute(new USBCameraViewer(turret));
 	}
 
 }
