10f60c9b6ee815bad4a0fdfd0c688e945e4dbb1a
AcademyHs Robotics <AcademyHs Robotics@LAPTOP-K0AG1H0S>
2016-03-25 10:22:21 -0500

    the new changes requested in competition

--- Smarter-Dashboard/src/org/usfirst/frc/team5431/components/MotorRPM.java
+++ Smarter-Dashboard/src/org/usfirst/frc/team5431/components/MotorRPM.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team5431.components;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.Rectangle;
@@ -36,10 +37,31 @@ public class MotorRPM {
 		flyrightspeed.setVisible(true);
 		f.add(flyrightspeed);
 		
+		final JLabel aimlefttitle = new JLabel("Calculated Speed");
+		aimlefttitle.setBounds(p.x, p.y+50, 200, 50);
+		aimlefttitle.setVisible(true);
+		f.add(aimlefttitle);
+		final JProgressBar aimleftspeed = new JProgressBar(0, 100);
+		aimleftspeed.setBounds(p.x+200, p.y+50, 500, 50);
+		aimleftspeed.setVisible(true);
+		f.add(aimleftspeed);
+		
+		final JLabel aimrighttitle = new JLabel("Calculated Speed");
+		aimrighttitle.setBounds(1217, 150, 200, 50);
+		aimrighttitle.setVisible(true);
+		f.add(aimrighttitle);
+		final JProgressBar aimrightspeed = new JProgressBar(0, 100);
+		aimrightspeed.setBounds(1417, 150, 500, 50);
+		aimrightspeed.setVisible(true);
+		f.add(aimrightspeed);
+		final Color defaultcolor = aimleftspeed.getForeground();
+		
 		seperate = seperated;
 		if(seperate){
 			flyleftspeed.setBounds(0,p.y,500,50);
 			flylefttitle.setBounds(500,p.y,200,50);
+			aimleftspeed.setBounds(0, p.y+50, 500, 50);
+			aimlefttitle.setBounds(500,p.y+50,200,50);
 		}
 
 		exe.execute(new Thread() {
@@ -49,6 +71,13 @@ public class MotorRPM {
 					sleep(1);
 					final int leftspeed = (int) SmarterDashboard.table.getNumber("FLY-RIGHT", 0.0),//the encoders are reversed
 							rightspeed = (int) SmarterDashboard.table.getNumber("FLY-LEFT", 0.0);
+					
+					final double autospeed = SmarterDashboard.table.getNumber("AUTO-AIM-SPEED", 0.0);
+					aimleftspeed.setValue(leftspeed);
+					aimrightspeed.setValue(rightspeed);
+					aimleftspeed.setString(leftspeed+" RPM");
+					aimrightspeed.setString(rightspeed+" RPM");
+					
 					flyleftspeed.setValue(leftspeed);
 					flyleftspeed.setString(leftspeed + " RPM");
 					flyrightspeed.setValue(rightspeed);
@@ -58,6 +87,8 @@ public class MotorRPM {
 						final Dimension size = flyrightspeed.getParent().getSize();
 						flyrightspeed.setBounds(size.width-500, p.y, 500, 50);
 						flyrighttitle.setBounds(size.width-700, p.y, 200, 50);
+						aimrightspeed.setBounds(size.width-500, p.y+50, 500, 50);
+						aimrighttitle.setBounds(size.width-700, p.y+50, 200, 50);
 					}
 				}
 				}catch(Throwable t){
